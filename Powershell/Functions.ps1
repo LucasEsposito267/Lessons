5b7c701730db5fde784d5382609aa99e1694b096
Lucas <lucasesposito267@gmail.com>
2017-01-04 21:27:48 -0300

    Adding function lessons

---
+++ Powershell/Functions.ps1
@@ -0,0 +1,34 @@
+﻿function Say-Hello($Name)
+{
+    Write-Output "Hello $name!"
+}
+
+##PASO A PASO
+
+# Creas la funcion con la palabra FUNCTION
+
+function
+
+# Le das nombre a la funcion
+
+function Get-ProcessByName
+
+# Le pones parametros a la funcion
+
+function Get-ProcessByName ($ProcessName)
+
+# Le agregas logica a la funcion
+
+function Get-ProcessByName ($ProcessName){
+    #La logica iria aca
+}
+
+#Terminas la funcion
+
+Function Get-ProcessByName ($ProcessName){
+
+    Write-Output "Getting all processes with name [$ProcessName]..."
+    
+    Get-Process -Name $ProcessName
+
+}
