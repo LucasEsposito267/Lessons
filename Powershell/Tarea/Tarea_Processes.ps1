ed2c38cfe7702fb800454cee5b01b9663f4d4d1c
Dalmirog <dalmiro.granias@gmail.com>
2017-01-11 19:29:25 -0300

    agregando tarea nueva

---
+++ Powershell/Tarea/Tarea_Processes.ps1
@@ -0,0 +1,21 @@
+﻿<#
+El otro dia te explique como el comando Get-Process te muestra todos los procesos, igual que el administrador de tareas. 
+
+En este archivo quiero que vayas poniendo abajo de cada linea verde el codigo que se te va pidiendo.
+
+Estos ejercicios van a implicar que googlees bastante por tu cuenta. Esto es bastante mas facil que hacer una funcion, pero te va a servir para ejercitar la busqueda por cuenta propia.
+
+#>
+
+
+##Usa Get-Process para mostrar en pantalla todos los procesos que empiezan con la letra "P". Acordate de usar el *
+
+
+
+##Abri un notepad por tu cuenta. Usa Get-Process para mostrarme el ID del proceso del notepad. Todos los procesos tienen un numero de ID que hace que puedas distinguir cada proceso si tenes varios que se llaman igual.
+
+$ID = #Aca iria Get Process
+
+Write-Output "El ID es: $ID" #Aca lo mostras en pantalla
+
+##Usa algun comando (vas a tener que googlear vos) para PARAR el proceso del notepad (es decir para cerrarlo).
