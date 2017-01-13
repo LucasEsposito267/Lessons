8b5b355c848ff9778dc96ec60e8eccdfc2f1de0c
LucasEsposito267 <lucasesposito267@gmail.com>
2017-01-13 01:01:23 -0300

    Update Tarea_Processes.ps1

--- Powershell/Tarea/Tarea_Processes.ps1
+++ Powershell/Tarea/Tarea_Processes.ps1
@@ -10,12 +10,18 @@ Estos ejercicios van a implicar que googlees bastante por tu cuenta. Esto es bas
 
 ##Usa Get-Process para mostrar en pantalla todos los procesos que empiezan con la letra "P". Acordate de usar el *
 
+Get-Process P* 
 
 
 ##Abri un notepad por tu cuenta. Usa Get-Process para mostrarme el ID del proceso del notepad. Todos los procesos tienen un numero de ID que hace que puedas distinguir cada proceso si tenes varios que se llaman igual.
 
-$ID = #Aca iria Get Process
-
-Write-Output "El ID es: $ID" #Aca lo mostras en pantalla
+$id = Get-process -name notepad | select -expandproperty Id
+Write-Host "El ID es: $ID"
 
 ##Usa algun comando (vas a tener que googlear vos) para PARAR el proceso del notepad (es decir para cerrarlo).
+
+stop-process -id 5080
+
+##o 
+
+stop-process -name notepad
