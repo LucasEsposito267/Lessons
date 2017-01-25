6b39d4bf4d245b470bbf0e9b2f7133f68d67c285
LucasEsposito267 <lucasesposito267@gmail.com>
2017-01-25 16:12:10 -0300

    Update BorradorCMDLetsIIS.ps1

--- Powershell/IIS/BorradorCMDLetsIIS.ps1
+++ Powershell/IIS/BorradorCMDLetsIIS.ps1
@@ -1,13 +1,34 @@
-function CreateAppPool($AppPoolName){
-    #Se encarga de que $AppPoolName exista
-}
+function CreateAppPool ($AppPoolName){
 
-function CreatePhysicalPath ($SourceFolder,$PhysicalPath){
-    #Se encarga de que $PhysicalPath exista y copia los contenidos de $SourceFolder
-}
+    If(Test-Path -Path IIS:\AppPools\$AppPoolName){
+        Write-Output "La Application Pool existe"
+    }
 
+    else{
+        New-WebAppPool -Name $AppPoolName -Verbose
+        Write-Output "La Application Pool no existia y ha sido creada"
+    }
+
+}
+function CreatePhysicalPath ($PhysicalPath,$SourceFolder){
+    
+    If (Test-Path -Path $PhysicalPath){
+        Write-Output "La ruta fisica existe"
+    }
+    else{
+        Copy-Item -Recurse -path $SourceFolder -destination $PhysicalPath -Verbose
+        Write-Output "La ruta fisica no existia y ha sido creada"
+    }
+}
 function CreateWebsite ($WebsiteName,$PhysicalPath){
-    #Se encarga de que $WebsiteName exista y que apunte a $PhysicalPath
+
+    if (Test-Path -Path IIS:\Sites\$WebsiteName){
+         Write-Output "La Pagina Web existe"
+    }
+
+    else{
+         New-Website -Name $WebsiteName -PhysicalPath $PhysicalPath -Verbose
+    }
 }
 
 function SetupEnvironment ($WebsiteName,$PhysicalPath,$SourceFolder){
