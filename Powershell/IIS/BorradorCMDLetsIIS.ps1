694f6ddcbec8f8afa4ff4f1bd8572f17c3f1aac8
Dalmirog <dalmiro.granias@gmail.com>
2017-01-25 18:04:02 -0300

    Final IIS functions

--- Powershell/IIS/BorradorCMDLetsIIS.ps1
+++ Powershell/IIS/BorradorCMDLetsIIS.ps1
@@ -1,4 +1,4 @@
-function CreateAppPool ($AppPoolName){
+function Create-AppPool ($AppPoolName){
 
     If(Test-Path -Path IIS:\AppPools\$AppPoolName){
         Write-Output "La Application Pool existe"
@@ -8,33 +8,42 @@ function CreateAppPool ($AppPoolName){
         New-WebAppPool -Name $AppPoolName -Verbose
         Write-Output "La Application Pool no existia y ha sido creada"
     }
-
 }
-function CreatePhysicalPath ($PhysicalPath,$SourceFolder){
+
+function Create-PhysicalPath ($PhysicalPath,$SourceFolder){
     
     If (Test-Path -Path $PhysicalPath){
-        Write-Output "La ruta fisica existe"
+        Write-Output "La ruta fisica existe. Borrando contenido Actual"
+        Remove-Item -Path $PhysicalPath\* -Recurse -Force
+        Copy-Item -Recurse -path $SourceFolder\* -destination $PhysicalPath -Verbose
     }
     else{
         Copy-Item -Recurse -path $SourceFolder -destination $PhysicalPath -Verbose
         Write-Output "La ruta fisica no existia y ha sido creada"
     }
 }
-function CreateWebsite ($WebsiteName,$PhysicalPath){
+
+function Create-Website ($WebsiteName,$PhysicalPath,$AppPoolName){
 
     if (Test-Path -Path IIS:\Sites\$WebsiteName){
-         Write-Output "La Pagina Web existe"
+         Write-Output "La Pagina Web existe. Modificando valores..."
+         Set-ItemProperty -Path IIS:\Sites\$WebsiteName -Name applicationPool -Value $AppPoolName -Verbose
+         Set-ItemProperty -Path IIS:\Sites\$WebsiteName -Name physicalPath -Value $PhysicalPath -Verbose
     }
 
-    else{
-         New-Website -Name $WebsiteName -PhysicalPath $PhysicalPath -Verbose
+    else{         
+         New-Website -Name $WebsiteName -PhysicalPath $PhysicalPath -Verbose -ApplicationPool $AppPoolName         
          Write-Output "La Pagina Web no existia y ha sido creada"
     }
 }
 
-function SetupEnvironment ($WebsiteName,$PhysicalPath,$SourceFolder){
-    #Esta es la funcion que orquesta todas las funcionalidades
-    CreateAppPool -AppPoolName $WebsiteName
-    CreatePhysicalPath -SourceFolder $SourceFolder -PhysicalPath $PhysicalPath
-    CreateWebsite -WebsiteName $WebsiteName -PhysicalPath $PhysicalPath
+function Setup-Environment ($WebsiteName,$PhysicalPath,$SourceFolder,$AppPoolName){
+    #CrearAppPool
+    Create-AppPool -AppPoolName $AppPoolName
+
+    #CrearPhysicalPath
+    Create-PhysicalPath -PhysicalPath $PhysicalPath -SourceFolder $SourceFolder
+
+    #CrearWebsite
+    Create-Website -WebsiteName $WebsiteName -PhysicalPath $PhysicalPath -AppPoolName $AppPoolName   
 }
