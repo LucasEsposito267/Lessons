70da6a4eabe1491788b36a9c0a5c5217fac5fbc7
Dalmirog <dalmiro.granias@gmail.com>
2017-02-01 16:31:29 -0300

    Mejorando un poco los mensajes de output para que incluyan el nombre del item con el que estamos trabajando

--- Powershell/IIS/Draft_DeleteFullIISWebsite.ps1
+++ Powershell/IIS/Draft_DeleteFullIISWebsite.ps1
@@ -2,11 +2,11 @@ function DeleteWebsite ($WebsiteName){
 
     if(Test-Path -Path IIS:\Sites\$WebsiteName){
          Remove-Website -name $websitename -verbose
-         Write-Output "El sitio web ha sido eliminado"
+         Write-Output "El sitio web [$WebsiteName] ha sido eliminado"
      }
     else{         
        
-         Write-Output "El sitio web no existe y no pudo ser eliminado"
+         Write-Output "El sitio web [$WebsiteName] no existe"
     }
 }
 
@@ -16,11 +16,11 @@ function DeleteApplicationPool ($AppPoolName){
     If(Test-Path -Path IIS:\AppPools\$AppPoolName){
     
         Remove-WebAppPool -name $AppPoolName -verbose
-        Write-Output "La Application Pool ha sido eliminada"
+        Write-Output "La Application Pool [$AppPoolName] ha sido eliminada"
     }
 
     else{
-        Write-Output "La Application Pool no existe y no pudo ser eliminada"
+        Write-Output "La Application Pool [$AppPoolName] no existe"
       
     }
 }
@@ -28,11 +28,11 @@ function DeleteApplicationPool ($AppPoolName){
 function DeletePhysicalPath ($PhysicalPath){
  
     If(Test-Path -Path $PhysicalPath){
-    Remove-Item -path $PhysicalPath -verbose
-    Write-Output "El directorio donde se alojaban los archivos fisicos del sitio ha sido eliminado"
+        Remove-Item -path $PhysicalPath -verbose
+        Write-Output "El directorio [$PhysicalPath] ha sido eliminado"
     }
     
     else{
-         Write-Output "El directorio no se pudo eliminar"
-         }
+         Write-Output "El directorio [$PhysicalPath] no existe"
+    }
 } 
