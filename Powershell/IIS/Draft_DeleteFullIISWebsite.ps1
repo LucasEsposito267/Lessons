4d0459d30d62b664df9863ed5bb35780d461551e
Dalmirog <dalmiro.granias@gmail.com>
2017-02-01 16:41:45 -0300

    agregando un poco mas de tarea

--- Powershell/IIS/Draft_DeleteFullIISWebsite.ps1
+++ Powershell/IIS/Draft_DeleteFullIISWebsite.ps1
@@ -36,3 +36,16 @@ function DeletePhysicalPath ($PhysicalPath){
          Write-Output "El directorio [$PhysicalPath] no existe"
     }
 } 
+
+<#
+4)
+Fijate que "Test-Path -path IIS:\Sites\$WebSiteName" unicamente funciona si antes te encargaste importar el modulo "WebADministration". Sin ese modulo, el DRIVE IIS:\ no estaria montado.
+
+Quiero que hagas una funcion que se llame "ImportModule" y que se encarge de importar el modulo que yo quiera si es que este no esta importado todavia.
+
+Para ejecutarla quiero hacer "ImportModule -ModuleName WebAdministration"
+
+Tip 1: Si para IMPORTAR un modulo corres "Import-Module", que comando crees que vas a tener que usar para ver si el modulo esta cargado?
+#>
+
+#5) Quiero que cambies todos los textos en español a Ingles :)
