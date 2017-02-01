1405429409e194cbcf4a60bd593fff836edf6fe2
Dalmirog <dalmiro.granias@gmail.com>
2017-01-31 21:20:13 -0300

    Adding DeleteFullIISWebsite homework

---
+++ Powershell/IIS/Draft_DeleteFullIISWebsite.ps1
@@ -0,0 +1,5 @@
+﻿#1) Una linea de codigo que borre el website de IIS.
+
+#2) Una linea de codigo que borre la app pool de IIS. Acordate que no podes borrar una AppPool si la esta usando un sitio.
+
+#3) Una linea de codigo que borre una carpeta con todos sus contenidos. Acordate que no podes borrar una carpeta si la esta usando un sitio.
