0f1b0089c3b09b1827142c9eae11a407aad403e4
LucasEsposito267 <lucasesposito267@gmail.com>
2017-02-02 02:23:41 -0300

    Update Draft_DeleteFullIISWebsite.ps1

--- Powershell/IIS/Draft_DeleteFullIISWebsite.ps1
+++ Powershell/IIS/Draft_DeleteFullIISWebsite.ps1
@@ -1,12 +1,24 @@
+function ImportModule ($ModuleName){
+
+     if(Get-Module -Name $ModuleName -verbose){
+    
+     } 
+     else{
+       Import-Module -name $ModuleName 
+       Write-Output "The module [$ModuleName] has been imported"
+     }
+
+}
+
 function DeleteWebsite ($WebsiteName){
 
     if(Test-Path -Path IIS:\Sites\$WebsiteName){
          Remove-Website -name $websitename -verbose
-         Write-Output "El sitio web [$WebsiteName] ha sido eliminado"
+         Write-Output "The Website [$WebsiteName] has been removed"
      }
     else{         
        
-         Write-Output "El sitio web [$WebsiteName] no existe"
+         Write-Output "The Website [$WebsiteName] doesn't exist"
     }
 }
 
@@ -16,11 +28,11 @@ function DeleteApplicationPool ($AppPoolName){
     If(Test-Path -Path IIS:\AppPools\$AppPoolName){
     
         Remove-WebAppPool -name $AppPoolName -verbose
-        Write-Output "La Application Pool [$AppPoolName] ha sido eliminada"
+        Write-Output "The Application Pool [$AppPoolName] has been removed"
     }
 
     else{
-        Write-Output "La Application Pool [$AppPoolName] no existe"
+        Write-Output "The Application Pool [$AppPoolName] doesn't exist"
       
     }
 }
@@ -28,24 +40,11 @@ function DeleteApplicationPool ($AppPoolName){
 function DeletePhysicalPath ($PhysicalPath){
  
     If(Test-Path -Path $PhysicalPath){
-        Remove-Item -path $PhysicalPath -verbose #Asegurate que esta linea borre los items SIEMPRE sin preguntarme si estoy seguro.
-        Write-Output "El directorio [$PhysicalPath] ha sido eliminado"
+        Remove-Item -path $PhysicalPath -verbose -force -recurse
+        Write-Output "The directory [$PhysicalPath] has been removed"
     }
     
     else{
-         Write-Output "El directorio [$PhysicalPath] no existe"
+         Write-Output "The directory [$PhysicalPath] doesn't exist"
     }
 } 
-
-<#
-4)
-Fijate que "Test-Path -path IIS:\Sites\$WebSiteName" unicamente funciona si antes te encargaste importar el modulo "WebADministration". Sin ese modulo, el DRIVE IIS:\ no estaria montado.
-
-Quiero que hagas una funcion que se llame "ImportModule" y que se encarge de importar el modulo que yo quiera si es que este no esta importado todavia.
-
-Para ejecutarla quiero hacer "ImportModule -ModuleName WebAdministration"
-
-Tip 1: Si para IMPORTAR un modulo corres "Import-Module", que comando crees que vas a tener que usar para ver si el modulo esta cargado?
-#>
-
-#5) Quiero que cambies todos los textos en español a Ingles :)
